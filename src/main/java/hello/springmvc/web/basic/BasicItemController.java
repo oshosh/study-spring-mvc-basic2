00c99025234ac6c12bc25ba05e65780377db3a5c
osh <dhtpgus7@gmail.com>
2024-04-07 23:17:42 +0900

    feat: MVC 등록

--- src/main/java/hello/springmvc/web/basic/BasicItemController.java
+++ src/main/java/hello/springmvc/web/basic/BasicItemController.java
@@ -9,8 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -37,11 +41,31 @@ public class BasicItemController {
     return "basic/item";
   }
 
-  @GetMapping("/{itemId}/edit")
-  public String edit(@PathVariable long itemId, Model model) {
-    Item item = itemRepository.findById(itemId);
-    model.addAttribute("item", item);
-    return "basic/editForm";
+  @GetMapping("/add")
+  public String addForm(Model model) {
+    model.addAttribute("itemDto", new Item());
+    return "basic/addForm";
+  }
+
+//  @PostMapping("/add/item-save")
+//  public String save(@RequestParam String itemName, @RequestParam Integer price,
+//      @RequestParam Integer quantity, Model model) {
+//    Item item = new Item();
+//    item.setItemName(itemName);
+//    item.setPrice(price);
+//    item.setQuantity(quantity);
+//    itemRepository.save(item);
+//    model.addAttribute("itemDto", item);
+//    return "redirect:/basic/item";
+//  }
+
+  @PostMapping("/add/item-save")
+  public String addItem(@ModelAttribute("ItemDto") Item itemValue, RedirectAttributes redirectAttributes) {
+    Item savedItem = itemRepository.save(itemValue);
+    System.out.println(savedItem);
+//    model.addAttribute("itemDto", item); // ModelAttribute에서 자동 추가됨
+//    redirectAttributes.addAttribute("itemId", savedItem.getId());
+    return "redirect:/basic/items";
   }
 
   @PostConstruct
